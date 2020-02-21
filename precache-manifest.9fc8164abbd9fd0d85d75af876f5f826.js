43d109dd56313bc72ebb470790db39ee4958834f
Stefan Eduard <eduard@pop-os.localdomain>
2020-02-21 12:14:19 +0100

    Updates

---
+++ precache-manifest.9fc8164abbd9fd0d85d75af876f5f826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e589134932be26a648747f3ceb069d",
+    "url": "/markdown-fcc/index.html"
+  },
+  {
+    "revision": "693edee747474d1bcc72",
+    "url": "/markdown-fcc/static/css/main.ddeefb65.chunk.css"
+  },
+  {
+    "revision": "a3116015a994baf7855f",
+    "url": "/markdown-fcc/static/js/2.8423d0d0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/markdown-fcc/static/js/2.8423d0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693edee747474d1bcc72",
+    "url": "/markdown-fcc/static/js/main.947d4639.chunk.js"
+  },
+  {
+    "revision": "243eddeef900e0ab5d9f",
+    "url": "/markdown-fcc/static/js/runtime-main.db4be05d.js"
+  }
+]);
